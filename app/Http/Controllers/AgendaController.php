638d5601d0211d2c6f3faf99f5d4ee5daf6e1fb4
Debora Souza <debora.souzalima22@gmail.com>
2024-12-25 12:02:47 -0300

    Inclusão inclusão das funções salvar, atualizar, listar todos, listar um e deletar no BD.

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -16,9 +16,72 @@ class AgendaController extends Controller
         $agenda->telefoneCelular = $request->telefoneCelular;
         $agenda->telefoneFixo = $request->telefoneFixo;
         $agenda->endereco = $request->endereco;
-        $agenda->telefoneFixo = 1;
+        $agenda->created_by = 1;
         $agenda->save();
 
         return response($agenda, 201);
+    }
+
+
+    // função que atualiza no bd
+    public function atualizarParcial($id, Request $request)
+    {
+        $agenda = Agenda::where('id', $id)->first();
+
+        if (isset($request->nome)) {
+            $agenda->nome = $request->nome;
+        }
+        if (isset($request->email)) {
+            $agenda->email = $request->email;
+        }
+        if (isset($request->telefoneCelular)) {
+            $agenda->telefoneCelular = $request->telefoneCelular;
+        }
+        if (isset($request->telefoneFixo)) {
+            $agenda->telefoneFixo = $request->telefoneFixo;
+        }
+        if (isset($request->endereco)) {
+            $agenda->endereco = $request->endereco;
+        }
+        $agenda->updated_by = 1;
+        $agenda->save();
+
+        return response($agenda, 200);
+    }
+
+    // função listar (todos)
+    public function listar()
+    {
+        $agenda = Agenda::get();
+
+        return response($agenda, 200);
+    }
+
+    // função listar/ ler um/ especifico
+    public function lerUm($id)
+    {
+        $code = 200;
+
+        $agenda = Agenda::where('id', $id)->first();
+
+        if ($agenda == null) {
+            $code = 404;
+        }
+
+        return response($agenda, $code);
+    }
+
+    // função deletar
+    public function deletar($id)
+    {
+        $code = 200;
+
+        $agenda = Agenda::where('id', $id)->delete();
+
+        if ($agenda == 0) {
+            $code = 404;
+        }
+
+        return response('', $code);
     }
 }
