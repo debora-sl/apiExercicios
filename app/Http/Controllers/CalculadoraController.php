6a51daf3685384ffc9695fdbd0141333343c492d
Debora Souza <debora.souzalima22@gmail.com>
2024-12-25 21:28:42 -0300

    Inclusão do salvar na operação Multiplicação.

--- app/Http/Controllers/CalculadoraController.php
+++ app/Http/Controllers/CalculadoraController.php
@@ -9,37 +9,39 @@ class CalculadoraController extends Controller
 {
     public function calcular(Request $request)
     {
-       $valorUm = $request->valorUm;
-       $valorDois = $request->valorDois;
-       $operacao = $request->operacao;
+        $valorUm = $request->valorUm;
+        $valorDois = $request->valorDois;
+        $operacao = $request->operacao;
 
-       if ($operacao == '+') {
-        $resultado = $valorUm + $valorDois;
+        if ($operacao == '+') {
+            $resultado = $valorUm + $valorDois;
 
-        // salvando no bd
-        static::guardarNoBanco($request, $resultado, null);
-
-        return response($resultado, 200);
-
-       }elseif ($operacao == '-') {
-        $resultado = $valorUm - $valorDois;
-
-        // salvando no bd
-        static::guardarNoBanco($request, $resultado, null);
-
-        return response($resultado, 200);
-       }elseif ($operacao == '*') {
-        $resultado = $valorUm * $valorDois;
-
-        return response($resultado, 200);
-       }elseif ($operacao == '/') {
-        if ( $valorDois == 0) {
-
-            $erro = "Não é possível dividir um número por Zero!";
             // salvando no bd
-            static::guardarNoBanco($request, null, $erro);
+            static::guardarNoBanco($request, $resultado, null);
 
-            return response( $erro, 422); // 422 - está tentando um dado improcessável
+            return response($resultado, 200);
+        } elseif ($operacao == '-') {
+            $resultado = $valorUm - $valorDois;
+
+            // salvando no bd
+            static::guardarNoBanco($request, $resultado, null);
+
+            return response($resultado, 200);
+        } elseif ($operacao == '*') {
+            $resultado = $valorUm * $valorDois;
+
+            // salvando no bd
+            static::guardarNoBanco($request, $resultado, null);
+
+            return response($resultado, 200);
+        } elseif ($operacao == '/') {
+            if ($valorDois == 0) {
+
+                $erro = "Não é possível dividir um número por Zero!";
+                // salvando no bd
+                static::guardarNoBanco($request, null, $erro);
+
+                return response($erro, 422); // 422 - está tentando um dado improcessável
             }
             $resultado = $valorUm / $valorDois;
 
@@ -47,13 +49,14 @@ class CalculadoraController extends Controller
             static::guardarNoBanco($request, $resultado, null);
 
             return response($resultado, 200);
-        }else{
-        $erro = "Operador invalido";
-        return response($erro, 422);
-       }
+        } else {
+            $erro = "Operador invalido";
+            return response($erro, 422);
+        }
     }
 
-    private static function guardarNoBanco($request, $resultado, $erro){
+    private static function guardarNoBanco($request, $resultado, $erro)
+    {
         // salvando no bd
         $calculadora = new Calculadora();
         $calculadora->valorUm = $request->valorUm;
